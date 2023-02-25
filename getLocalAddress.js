1c19782e3cf622a4b894ddb524b9fb78da455110
Nequeo2000 <126255817+Nequeo2000@users.noreply.github.com>
2023-02-25 19:19:53 +0100

    Update getLocalAddress.js

--- getLocalAddress.js
+++ getLocalAddress.js
@@ -1,7 +1,7 @@
 const { networkInterfaces } = require('os');
 
 function getIPv4Address() {
-    const nets = networkInterfaces()["wlp2s0"];
+    const nets = networkInterfaces()["wlp2s0"] || networkInterfaces()["eth0"];
     const results = {}; // Or just '{}', an empty object
 
     for (let index in nets) {
