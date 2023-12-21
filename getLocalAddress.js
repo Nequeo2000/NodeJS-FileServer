a891222db41d2cef521c179d517c411a6323fef0
Patrick Sonnenberg <126255817+Nequeo2000@users.noreply.github.com>
2023-12-21 12:03:22 +0100

    Update getLocalAddress.js

--- getLocalAddress.js
+++ getLocalAddress.js
@@ -8,6 +8,7 @@ function getIPv4Address() {
         let nets = net_list[net_key];
         for (let index in nets) {
             let netInfo = nets[index];
+            console.log(netInfo["address"]);
             if (netInfo["family"] == "IPv4") {
                 return netInfo["address"];
             }
